64c4e2d8d854b38f04faa9a84860f4bd10a52f5e
Fi-Loy <david.brunner00@gmail.com>
2026-09-19 08:39:55 -0400

    First commit
    Project folder for a homemade C library as practice and support for other C projects
    
    First commit is to save a working copy of a hashtable data structure. reworking the structure to use an array of pointers rather than a raw array

---
+++ hashtable/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TABLESIZE 101
+
+static struct hashBin {
+    char* key;
+    double value;
+    struct hashBin* next;
+};
+
+static struct hashBin table[TABLESIZE];
+
+static int hash(char* keyPointer) {
+    unsigned int hash = 0;
+
+    while (*++keyPointer != '\0'){ 
+        hash = hash << 1;
+        hash = hash ^ *keyPointer;
+    }
+
+    return hash % TABLESIZE;
+}
+
+//Places the key-value pair in the hashmap.
+//If an item already exists with the same key,
+//it's value is updated.
+void insert(char* key, double value) {
+    int index = hash(key);
+    struct hashBin* hbp = &table[index];
+
+    while (hbp->key != NULL) {
+        if (strcmp(key, hbp->key) == 0)
+            break;
+        hbp->next = malloc(sizeof(struct hashBin));
+        hbp = hbp->next;
+    }
+    hbp->key = key;
+    hbp->value = value;
+}
+
+//Searches for key in hashmap and returns the value 
+//stored to the value pointer passed.
+//Returns 0 if successful, returns -1 elsewise
+int search(char* key, double* value){
+    int index = hash(key);
+    struct hashBin* hbp = &table[index];
+
+    while (hbp->key != NULL) 
+        if (strcmp(key, hbp->key) == 0){
+            *value = hbp->value;
+            return 0;
+        }
+        else if (hbp->next != NULL)
+            hbp = hbp->next;
+        else
+            break;
+    return -1;
+}
+
+//Finds if a value is associated with the key given 
+//and deletes it. Returns 0 if a value was found,
+//-1 elsewise.
+//int delete(char* key) {
+//    int index = hash(key);
+//    struct hashBin* hbp = &table[index];
+//
+//    while (hbp->key != NULL) 
+//        if (strcmp(key, hbp->key) == 0)
+//            if (hbp->next == NULL) {
+//                hbp->key == NULL;
+//                hbp->value == 0;
+//                return 0;
+//            } else {
+//                *hbp = *(hbp->next);
+//            }
+//}
+
+main() {
+    printf("testing the hash function\n");
+    char key1[] = "Recipe Name";
+    char key2[] = "Grain Matrix";
+    printf("Key: %s, hash:%d\n", key1, hash(key1));
+    printf("Key: %s, hash:%d\n", key2, hash(key2));
+
+    insert("OG", 1.062);
+    insert("OG", 1.056);
+    double result;
+    
+    if (search("OG", &result) == 0)
+        printf("the value associated with the key %s, is %f\n","OG", result);
+    else
+        printf("key does not exist in the hashmap");
+    if (search("FG", &result) == 0)
+        printf("the value associated with the key %s, is %f\n","OG", result);
+    else
+        printf("key does not exist in the hashmap\n");
+
+    insert("FG", 1.008);
+}
